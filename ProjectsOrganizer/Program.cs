aa7c7e16c51c81c4cc37b9fa2aa60d45842f14f9
m4t3n5z <mz3pr0gr4mmm3r@gmail.com>
2021-01-03 23:15:28 +0100

    Add 5-ClearConsol and repair info

--- ProjectsOrganizer/Program.cs
+++ ProjectsOrganizer/Program.cs
@@ -10,28 +10,34 @@ namespace ProjectsOrganizer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Życie to pierdolony Projekt\nPodaj numer:\n" +
+            var info = "Życie to pierdolony Projekt\n\n\n" +
+              "INFO\n" +
               "1-Wyswietl liste projektów\n" +
               "2-Dodaj projekt\n" +
               "3-Usun projekt\n" +
               "4-Dodaj update\n" +
-              "5-Wyjdz z apki\n");
+              "5-ClearConsole\n" +
+              "6-Wyjdz z apki\n";
+
+            Console.WriteLine(info);
 
             var ifExit = false;
-            string text = System.IO.File.ReadAllText(@"../../ProjectsData/DATA.txt");
+            //string text = System.IO.File.ReadAllText(@"../../ProjectsData/DATA.txt");
             while (!ifExit)
             {
                 var option = int.Parse(Console.ReadLine());
 
                 switch (option)
                 {
-                    case 1: Console.WriteLine(text); break;
-                    case 2: break;
+                    case 1: Console.WriteLine("Oto nazwy projektów wraz z ich numeracją\n" +
+                        System.IO.File.ReadAllText(@"../../ProjectsData/DATA.txt")); break;
+                    case 2: 
+                        break;
                     case 3: break;
                     case 4: break;
-                    case 5: ifExit = true; break;
-                    default:
-                        break;
+                    case 5: Console.Clear(); Console.WriteLine(info); break;
+                    case 6: ifExit = true; break;
+                    default: Console.WriteLine("Podałeś złą liczbę, Patrz INFO"); break;
                 }
             }
         }
