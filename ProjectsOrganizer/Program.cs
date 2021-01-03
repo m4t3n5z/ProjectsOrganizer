7039e84092e5334fc750ebb5f8cb2e3de9c3175e
m4t3n5z <mz3pr0gr4mmm3r@gmail.com>
2021-01-03 22:53:38 +0100

    Add project file

---
+++ ProjectsOrganizer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectsOrganizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Życie to pierdolony Projekt\nPodaj numer:\n" +
+              "1-Wyswietl liste projektów\n" +
+              "2-Dodaj projekt\n" +
+              "3-Usun projekt\n" +
+              "4-Dodaj update\n" +
+              "5-Wyjdz z apki");
+
+            var ifExit = false;
+            string text = System.IO.File.ReadAllText(@"../../../data.txt");
+            while (!ifExit)
+            {
+                var option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1: Console.WriteLine(text); break;
+                    case 2: break;
+                    case 3: break;
+                    case 4: break;
+                    case 5: ifExit = true; break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
